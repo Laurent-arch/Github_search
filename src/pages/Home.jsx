834f8d8e546f66f73c89a58c1cbc13b0b802e177
laurent Garcia <garcia.laurent@hotmail.fr>
2022-02-26 16:34:38 +0200

    final commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../components/users/UserResults"
+import UserSearch from '../components/users/UserSearch'
+
+const Home = () => {
+  return (
+    <>
+        <UserResults />
+        <UserSearch />
+    </>
+  )
+}
+
+export default Home
